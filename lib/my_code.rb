94e1ebfa9a45ada7c2cba2663dceb0ab27f5ea61
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-26 03:42:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map (source_array)
+  newmap = []
+  i = 0
+  while i < source_array.length do
+    newmap.push(yield(source_array[i]))
+    i += 1
+  end
+ return newmap 
+end
+
+def reduce (source_array, starting_value = 0)
+  
+  while i < source_array.length do
+    
+end
+
